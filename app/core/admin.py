8ef1cdb7483005f41ac8ca7c6c24a2c625ef6dba
Elsaeed Ahmed <elsaeedahmed97@gmail.com>
2022-08-31 09:37:00 +0200

    Add the ability to create new users

--- app/core/admin.py
+++ app/core/admin.py
@@ -39,3 +39,17 @@ class UserAdmin(BaseUserAdmin):
         )
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
